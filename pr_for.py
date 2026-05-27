d118c119c84f79557d3add7185d9269f6fc4b29f
HannaOdud <annodud@gmail.com>
2026-05-27 19:53:14 +0100

    fifth

---
+++ pr_for.py
@@ -0,0 +1,20 @@
+'''
+print("Виведи таблицю множення від 1 до 10.")
+num = int(input("Write your num: "))
+tot = 0
+for i in range(1, 11):
+    tot = i*num
+    print(f"{i}*{num} = {tot}")'''
+
+'''print("Виведи кожну букву окремо через for.")
+text = "Python"
+for i in text:
+    print(i)'''
+
+'''print("Порахуй, скільки букв \"a\" є у слові.")
+text = "banana"
+tot = 0
+for i in text:
+    if i == "a":
+        tot = tot + 1
+print(f"Tot \"a\" in text: {tot}")'''
--- prac_while_fibonacci.py
+++ prac_while_fibonacci.py
@@ -2,6 +2,9 @@ print("Fibonacci. Виведи перші n чисел Фібоначчі чер
 n = int(input("Write a number: "))
 i = 1
 #tot = 0
+a = 0
+b = 1
 while i <= n:
-    
+   
     i += 1
+
