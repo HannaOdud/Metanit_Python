ff15be080d270bcd95e15ca893082c78b4045411
HannaOdud <annodud@gmail.com>
2026-06-25 23:11:38 +0100

    list_prac(4)

---
+++ list/list_missing_two_num.py
@@ -0,0 +1,5 @@
+print("A list contains numbers from 1 to 20, but two numbers are missing.")
+print("Find the missing number efficiently.")
+
+numbers = [1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,  20]
+
