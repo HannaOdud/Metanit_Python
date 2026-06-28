34ae86e0cc47308094916d5ccab905c2d2aacf55
HannaOdud <annodud@gmail.com>
2026-06-28 21:54:50 +0100

    20_tasks_in_progress

---
+++ def/def_gpt_20_tasks.py
@@ -0,0 +1,33 @@
+#def_gpt_20_tasks
+#1
+def double(number):
+    return number * 2
+print(double(3))
+
+#2
+def subtract(a, b):
+    return a-b
+print(subtract(7, 5))
+
+#3
+def is_even(number):
+    if number%2 == 0:
+        return True
+    else:
+        return False
+print(is_even(2))
+print(is_even(3))
+
+#4
+def minimum(a, b):
+    if a < b:
+        return a
+    else: 
+        return b
+print(minimum(1,3))
+
+#5
+def greeting(name):
+    return name
+print(greeting("Привіт, Олено!"))
+
