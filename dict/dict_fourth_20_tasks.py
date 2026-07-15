d6ec154e5aa91ba1ac7bb6587554d838a0ad41aa
HannaOdud <annodud@gmail.com>
2026-07-15 19:29:05 +0100

    finished

--- dict/dict_fourth_20_tasks.py
+++ dict/dict_fourth_20_tasks.py
@@ -214,8 +214,39 @@ def unique_symbol(word):
             unique_chars.append(char)
     return unique_chars
         
-print(unique_symbol("banana"))            
+print(unique_symbol("banana"))     
 
- 
+#19 Порахуй голосні та приголосні. Функція повинна повернути словник:
+def count_vow_con(word):
+    con = 0
+    vow = 0
+    for char in word:
+        if char in "aeiou":
+            con += 1
+        else:
+            vow += 1
+    res_dict = {"vowels": vow,
+                "consonants": con}
+    return res_dict
+print(count_vow_con("listen"))
+
+#20 Напиши функцію, яка поверне список імен усіх студентів із найвищою оцінкою:
+students = [
+    {"name": "Anna", "score": 95},
+    {"name": "Ivan", "score": 81},
+    {"name": "Olena", "score": 90},
+    {"name": "Petro", "score": 95}
+]
+def best_students(students):
+    res_list = []
+    max_score = 0
+    for dict in students:
+        if dict["score"] > max_score:
+            max_score = dict["score"] 
+    for dict in students:
+        if dict["score"] == max_score:
+            res_list.append(dict["name"])
+    return res_list
+print(best_students(students))
    
 
