af1be4a7b8ae27c6658b81f946cfdd0652c2c3a5
HannaOdud <annodud@gmail.com>
2026-07-21 23:38:20 +0100

    some changes

--- string/str_second_30_tasks.py
+++ string/str_second_30_tasks.py
@@ -29,10 +29,19 @@ print(count_spaces(" I_12345I_!#O"))
 def remove_digits(text):
     res = ""
     for item in text:
-        if item.isalpha():
+        if not item.isalpha():
             res += item
     return res
 print( remove_digits("ab12cd34"))
+
+
+def remove_digits(text):
+    res = ""
+    for item in text:  
+        if not item.isdigit():
+            res += item
+    return res
+print( remove_digits("ab-12 cd!"))
 
 #5 Повертає лише цифри.
 def only_digits(text):
@@ -46,11 +55,27 @@ print(only_digits("My phone 12345"))
 # 6 Повертає кількість слів без використання split().
 def count_words(text):
     count = 0
+    text = text.strip()
     for item in text:
         if item == " ":
             count = count +1
-    return count
+    return count+1
 print(count_words(" My phone is 12345"))
+
+def count_words(text):
+    res = ""
+    text = text.strip()
+    for i in range(len(text)):
+        if text[i] != " " or text[i-1] != " ":
+            res += text[i]
+    count = 0
+    for item in res:
+        if item == " ":
+            count += 1
+    return count
+print(count_words(" hello here, dont look like a stingy scrooge"))
+    
+
 
 #7 Повертає перше слово без split().
 def first_word(text):
@@ -75,6 +100,12 @@ def last_word(text):
     return res[::-1]
   
 print(last_word(" My phone 12345"))
+
+def last_word2(text):
+    l_in = text.rfind(" ")
+    l_el = text[l_in+1:]
+    return l_el
+print(last_word2(" My phone 1234567890"))
 
 #9 
 def reverse_words(text):
@@ -116,16 +147,16 @@ def last_word(text):
     return res
 print(last_word(" My phone 12345")) '''
 
-'''#10 Повертає найдовше слово.
+#10 Повертає найдовше слово.
 def longest_word(text):
     res = {}
+    m = ""
     list_text = text.split()
     for item in list_text:
         res[item] = len(item)
-    for key, value in res.items():
-        max(value)
-    return max(value)
+    m = max(res, key=res.get)
+    return m
     
-print(longest_word("I love Python"))  '''
+print(longest_word("I love Cabbage"))  
 
 
