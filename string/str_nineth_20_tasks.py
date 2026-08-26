61d4d65b313eef1b88e2a42c867ead2b6cc21588
HannaOdud <annodud@gmail.com>
2026-08-26 13:04:49 +0100

    1-4 are done

--- string/str_nineth_20_tasks.py
+++ string/str_nineth_20_tasks.py
@@ -23,11 +23,83 @@ print("2.--------------------------------------------")
 def words_with_max_frequency(text):
     words = text.split()
     words_count = {}
+    res = []
     for word in words:
         words_count[word] = words_count.get(word, 0)+1
     #return words_count
-    first_common_word = ""
-    second_common_word = ""
     max_common_count = 0
     for key,value in words_count.items():
         if value > max_common_count:
+            max_common_count = value
+    for key,value in words_count.items():
+        if value == max_common_count:
+            res.append(key)
+    return res
+print(words_with_max_frequency("cat dog cat bird dog"))
+
+print("3.--------------------------------------------")
+# Найдовше слово серед тих, що повторюються
+# Тобто слово повинно зустрічатися мінімум двічі, а серед таких потрібно знайти найдовше.
+def longest_repeated_word(text):
+    words = text.split()
+    words_count = {}
+    res = []
+    for word in words:
+        words_count[word] = words_count.get(word, 0)+1
+    #return words_count
+    for key,value in words_count.items():
+            if value >= 2:
+                res.append(key)
+    longest = max(res, key=len)
+    return longest
+print(longest_repeated_word("cat dog bird cat rabbit bird dog"))
+
+#OR
+def longest_repeated_word2(text):
+    words = text.split()
+    words_count = {}
+    res = []
+    for word in words:
+        words_count[word] = words_count.get(word, 0)+1
+    #return words_count
+    for key,value in words_count.items():
+            if value >= 2:
+                res.append(key)
+    longest = ""
+    len_longest = 0
+    for word in res:
+        if len(word) > len_longest:
+            len_longest = len(word)
+            longest = word
+    return longest
+print(longest_repeated_word2("cat dog bird cat rabbit bird dog"))
+
+#OR
+def longest_repeated_word3(text):
+    words = text.split()
+    words_count = {}
+    for word in words:
+        words_count[word] = words_count.get(word, 0)+1
+    #return words_count
+    longest = ""
+    for key,value in words_count.items():
+            if value >= 2 and len(key) > len(longest):
+                longest = key   
+    return longest
+print(longest_repeated_word3("cat dog bird cat rabbit bird dog"))
+
+
+print("4.---------------------------------------------------")
+# Коротше за середнє. Повертає список слів, довжина яких менша за середню довжину всіх слів.
+def shorter_than_average(text): 
+    words = text.split()
+    sum_len = 0
+    for word in words:
+        sum_len += len(word)
+    avg = sum_len /len(words)
+    res = []
+    for word in words:
+        if len(word) < avg:
+            res.append(word)
+    return res
+print(shorter_than_average("cat dog bird cat rabbit bird dog"))     
