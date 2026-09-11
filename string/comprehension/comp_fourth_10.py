f84578a19edc902d3f2fb90490b85d309c93dc27
HannaOdud <annodud@gmail.com>
2026-09-11 14:02:38 +0100

    all done

--- string/comprehension/comp_fourth_10.py
+++ string/comprehension/comp_fourth_10.py
@@ -96,3 +96,97 @@ print(res)
 #OR
 res = {num for num in list1 if num in list2}
 print(res)
+
+print("8.------------------")
+students = {
+    "Anna": [85, 90, 78],
+    "John": [60, 72, 65],
+    "Mike": [95, 88, 92],
+    "Kate": [70, 75, 80]
+}
+#step1
+avg_m = {key: sum(value)/len(value) for key,value in students.items() }
+print(avg_m)
+
+#step2
+max_v = 0
+max_k = ""
+for key,value in avg_m.items():
+    if value > max_v:
+        max_v = value
+        max_k = key
+print(max_k)
+
+
+print("9.-----------------")
+words = [
+    "Apple",
+    "banana",
+    "APPLE",
+    "orange",
+    "banana",
+    "kiwi",
+    "Orange"
+]
+freq = {}
+for word in words:
+    clean_word = word.lower().strip(",.!?;: ")
+    freq[clean_word] = freq.get(clean_word,0)+1
+print(freq)
+ls = []
+for key,value in freq.items():
+    if value == 1:
+        ls.append(key)
+print(ls[0])
+
+
+print("10.---------------")
+text = """
+Python is easy to learn.
+Python is powerful.
+Python is popular.
+Java is powerful too.
+Python and Java are popular.
+"""
+#step1
+words = text.split()
+clean_words = [word.lower().strip(",.!?;: ") for word in words]
+print(clean_words)
+
+#step2
+freq = {}
+for word in clean_words:
+    freq[word] = freq.get(word,0)+1
+print(freq)
+
+#step3 
+max_w = max(freq, key=freq.get)
+print(max_w)
+
+#step4
+repeated = [key for key,value in freq.items() if value > 1]
+print(repeated)
+
+#step5
+ls = []
+for word in clean_words:
+    if freq[word] == 1:
+        ls.append(word)
+        break
+print(ls[0])
+
+#step6
+res = {word:len(word) for word in clean_words if len(word)>4}
+print(res)
+
+#step7
+desc = {k: v for k, v in sorted(freq.items(), reverse=True, key=lambda item: item[1])}
+print(desc)
+
+
+
+
+
+
+
+
